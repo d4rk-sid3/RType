1675de138c2e7d7fec376543bb9df6e3ccf216eb
amourguidi <amour.guidi@epitech.eu>
2025-09-22 19:17:28 +0100

    add : adding of the Networkmanager class

---
+++ src/rtype_server/header/Network.hpp
@@ -0,0 +1,55 @@
+/*
+** EPITECH PROJECT, 2025
+** G-CPP-500-COT-5-1-rtype-8
+** File description:
+** Network
+*/
+
+#ifndef NETWORK_HPP_
+#define NETWORK_HPP_
+    #include <asio.hpp>
+    #include <sys/socket.h>
+    #include <arpa/inet.h>
+    #include <netinet/in.h>
+    #include <netinet/ip.h>
+    #include <stdio.h>
+    #include <unistd.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <poll.h>
+    #include <sys/stat.h>
+    #include <fcntl.h>
+    #include <stdbool.h>
+    #include <ctype.h>
+    #include <time.h>
+    #include <limits.h>
+    #include <string>
+    #include <sstream>
+    #include <iostream>
+    #include <filesystem>
+    #include <bits/stdc++.h>
+    #include <cstdlib>
+    #include <vector>
+    #include <thread>
+    #include <sys/ioctl.h>
+
+
+    class NetworkManager {
+        public:
+            NetworkManager(int port);
+            ~NetworkManager();
+            void run();
+            void Receive();
+            void Sendit(const std::string &msg, const asio::ip::udp::endpoint& client);
+
+        protected:
+
+        private: 
+            asio::io_context context;
+            asio::ip::udp::socket socket;
+            std::array<char, 1024> buff;
+            asio::ip::udp::endpoint client_;
+            bool isrunning;
+    };
+
+#endif /* !NETWORK_HPP_ */
