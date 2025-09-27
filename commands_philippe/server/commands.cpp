8a1008b5629a672b85577835c61a412901b84f10
Akanphi <akande-philippe.abiodoun@epitech.eu>
2025-09-27 05:37:09 +0100

    add: Encoded the message for the command CheckpointResponse

--- commands_philippe/server/commands.cpp
+++ commands_philippe/server/commands.cpp
@@ -85,3 +85,35 @@ std::vector<uint8_t> encodeBeatBossResponse(const BeatBossResponse& pos) {
 
     return buffer;
 }
+
+std::vector<uint8_t> encodeCheckpointResponse(const CheckpointResponse& pos) {
+    std::vector<uint8_t> buffer;
+
+    buffer.emplace_back(0x31);
+
+    buffer.emplace_back((pos.player_id >> 24) & 0xFF);
+    buffer.emplace_back((pos.player_id >> 16) & 0xFF);
+    buffer.emplace_back((pos.player_id >> 8) & 0xFF);
+    buffer.emplace_back(pos.player_id & 0xFF);
+
+    buffer.emplace_back((pos.checkpoint_id >> 24) & 0xFF);
+    buffer.emplace_back((pos.checkpoint_id >> 16) & 0xFF);
+    buffer.emplace_back((pos.checkpoint_id >> 8) & 0xFF);
+    buffer.emplace_back(pos.checkpoint_id & 0xFF);
+
+    buffer.emplace_back((pos.player_position.x >> 8) & 0xFF);
+    buffer.emplace_back(pos.player_position.x & 0xFF);
+    buffer.emplace_back((pos.player_position.y >> 8) & 0xFF);
+    buffer.emplace_back(pos.player_position.y & 0xFF);
+
+    buffer.emplace_back((pos.timestamp.milliseconds >> 56) & 0xFF);
+    buffer.emplace_back((pos.timestamp.milliseconds >> 48) & 0xFF);
+    buffer.emplace_back((pos.timestamp.milliseconds >> 40) & 0xFF);
+    buffer.emplace_back((pos.timestamp.milliseconds >> 32) & 0xFF);
+    buffer.emplace_back((pos.timestamp.milliseconds >> 24) & 0xFF);
+    buffer.emplace_back((pos.timestamp.milliseconds >> 16) & 0xFF);
+    buffer.emplace_back((pos.timestamp.milliseconds >> 8) & 0xFF);
+    buffer.emplace_back(pos.timestamp.milliseconds & 0xFF);
+
+    return buffer;
+}
