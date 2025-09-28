fb647424ba9a4e29112e53c29893260293951967
Aimane01 <aimane.alassane@epitech.eu>
2025-09-29 01:50:00 +0200

    add: creating User class ctor that takes a plain_password and create a user with password_hash using libsodium library

--- network/server/server.hpp
+++ network/server/server.hpp
@@ -10,6 +10,9 @@ class User_Stats {
         int _games_won = 0;
 
     public:
+        UserStats() = default;
+        ~UserStats(){};
+
         int getNbGamesPlayed() const { return _games_played; }
         int getNbGamesWon() const { return _games_won; }
 
@@ -21,32 +24,76 @@ class User_Stats {
 };
 
 class User {
-    private:
-        size_t id;
-        std::string username;
-        std::string password_hash;
-        std::array<uint8_t, crypto_auth_hmacsha256_BYTES> auth_token{};
-        std::array<uint8_t, crypto_auth_hmacsha256_BYTES> session_token{};
-        std::string auth_token_hex;
-        std::string session_token_hex;
-        time_t last_login_at = 0;
-        std::string client_hash;
-        User_Stats stats;
-
     public:
-        size_t getId() const { return id; }
-        void setId(size_t uid) { id = uid; }
-        const std::string& getUsername() const { return username; }
-        void setUsername(const std::string& uname) { username = uname; }
-        const std::string& getPasswordHash() const { return password_hash; }
-        void setPasswordHash(const std::string& hash) { password_hash = hash; }
-        const std::string& getAuthTokenHex() const { return auth_token_hex; }
-        void setAuthTokenHex(const std::string& token) { auth_token_hex = token; }
-        const std::string& getSessionTokenHex() const { return session_token_hex; }
-        void setSessionTokenHex(const std::string& token) { session_token_hex = token; }
-        time_t getLastLoginAt() const { return last_login_at; }
-        void setLastLoginAt(time_t t) { last_login_at = t; }
-        const std::string& getClientHash() const { return client_hash; }
-        void setClientHash(const std::string& h) { client_hash = h; }
-        User_Stats& getStats() { return stats; }
+        using Token128 = std::array<uint8_t,16>;
+        using Token256 = std::array<uint8_t,32>;
+
+        User(size_t id, std::string username, std::string plain_password)
+        : id_(id), username_(std::move(username)) 
+        {
+            if (sodium_init() < 0) {
+                throw std::runtime_error("Failed to initialize libsodium");
+            }
+            char hashed_password[crypto_pwhash_STRBYTES];
+            sodium_mlock(const_cast<char*>(plain_password.data()), plain_password.size());
+
+            if (crypto_pwhash_str(
+                    hashed_password,
+                    plain_password.c_str(),
+                    plain_password.size(),
+                    crypto_pwhash_OPSLIMIT_MODERATE,
+                    crypto_pwhash_MEMLIMIT_MODERATE) != 0) 
+            {
+                sodium_munlock(const_cast<char*>(plain_password.data()), plain_password.size());
+                throw std::runtime_error("Failed to hash password");
+            }
+            sodium_munlock(const_cast<char*>(plain_password.data()), plain_password.size());
+            sodium_memzero(const_cast<char*>(plain_password.data()), plain_password.size());
+            _password_hash = hashed_password;
+        }
+        void setAuthToken(const Token256& tok) {
+            _auth_token = tok;
+            _auth_token_hex = bytesToHex(auth_token.data(), auth_token.size());
+        }
+        void setSessionToken(const Token128& tok) {
+            _session_token = tok;
+            _session_token_hex = bytesToHex(_session_token.data(), _session_token.size());
+        }
+        void setClientHash(const Token256& ch) {
+            _client_hash = ch;
+        }
+        void setName(const std::string &name) {
+            _username = name;
+            return;
+        }
+
+    private:
+        size_t _id = 0;
+        std::string _username;
+        std::string _password_hash;
+        Token256 _auth_token{};
+        Token128 _session_token{};
+        std::string _auth_token_hex;
+        std::string _session_token_hex;
+        std::time_t _last_login_at = 0;
+        Token256 _client_hash;
+        User_Stats _stats;
 };
+
+class UserManager {
+    private:
+        std::map<std::string, sf::Texture*> m_textureMap;
+        static TextureManager* s_pInstance;
+        TextureManager(){};
+    public:
+        ~TextureManager(){};
+        static TextureManager* Instance() {
+            if (s_pInstance == nullptr) {
+                s_pInstance = new TextureManager();
+            }
+            return s_pInstance;
+        }
+        bool load(std::string fileName,std::string id);
+        sf::Texture* getTexture(const std::string& id);     
+};
+#endif /* defined(_Game_) */
