6a7d16d5d4aa9990af151400b93903bcbd5f4068
Akanphi <akande-philippe.abiodoun@epitech.eu>
2025-09-29 13:25:57 +0100

    add: Decode the message from the PlayerStateResponse command

---
+++ commands_philippe/server/decodeur.cpp
@@ -0,0 +1,17 @@
+#include "../include/commands.hpp"
+
+std::vector<uint8_t> decodePlayerStateResponse(const const std::vector<uint8_t>& buffer)
+{
+    PlayerStateResponse pos;
+
+    if (buffer[0] != 0x28) {
+        throw std::runtime_error("Type de message invalide !");
+    }
+
+    pos.player_id = (buffer[1] << 24) | (buffer[2] << 16) | (buffer[3] << 8) | buffer[4];
+    pos.remaining_health = (buffer[5] << 8) | buffer[6];
+    pos.score = pos.player_id = (buffer[7] << 24) | (buffer[8] << 16) | (buffer[9] << 8) | buffer[10]; 
+    pos.current_level = buffer[11];
+    pos.state = buffer[12];
+    return pos;
+}
