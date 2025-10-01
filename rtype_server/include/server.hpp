71f4dd9915478fd6ca44f2e5a0da183b76158a6b
amourguidi <amour.guidi@epitech.eu>
2025-10-01 20:58:38 +0100

    add: Adding of endcode and decode functions file in server folder

--- rtype_server/include/server.hpp
+++ rtype_server/include/server.hpp
@@ -15,20 +15,58 @@ class Server {
     MoveResponse response;
     MoveRequest move;
     NetworkManager server_;
-    PickupItemResponse item;
-    PlayerStateResponse p_response;
-    PlayerStateResponse2 p_response2;
- 
+
   public:
     Server(int p);
-    MoveResponse getMove();
-    MoveResponse recupMove(const std::pair<std::vector<uint8_t>, asio::ip::udp::endpoint> &a, MoveRequest &m);
-
-    PickupItemResponse recupItem(const std::pair<std::vector<uint8_t>, asio::ip::udp::endpoint> &a);
-    PlayerStateResponse player_r(const std::pair<std::vector<uint8_t>, asio::ip::udp::endpoint> &a);
-    PlayerStateResponse2 player_r2(const std::pair<std::vector<uint8_t>, asio::ip::udp::endpoint> &a);
-
     ~Server();
+    MoveResponse getMove();
+    MoveResponse recupMove(
+        const std::pair<std::vector<uint8_t>, asio::ip::udp::endpoint>& a,
+        MoveRequest& m
+    );
+
+    // update
+    MoveResponse
+    updateMoveResponse(const MoveResponse& oldpos, const MoveResponse& pos);
+    ShootResponse
+    updateShootResponse(const ShootResponse& oldpos, const ShootResponse& pos);
+    PickupItemResponse updatePickupItemResponse(
+        const PickupItemResponse& oldpos, const PickupItemResponse& pos
+    );
+
+    // decodeur
+    MoveRequest decodeMoveRequest(const std::vector<uint8_t>& buffer);
+    ShootRequest decodeShootRequest(const std::vector<uint8_t>& buffer);
+    PickupItemResquest
+    decodePickupItemResquest(const std::vector<uint8_t>& buffer);
+    GamePausedRequest decodeGamePausedRequest(const std::vector<uint8_t>& buffer
+    );
+
+    // encodeur
+    std::vector<uint8_t> encodeMoveResponse(const MoveResponse& pos);
+    std::vector<uint8_t> encodeShootResponse(const ShootResponse& pos);
+    std::vector<uint8_t> encodePickupItemResponse(const PickupItemResponse& pos
+    );
+    std::vector<uint8_t>
+    encodePlayerStateResponse(const PlayerStateResponse& pos);
+    std::vector<uint8_t>
+    encodePlayerGameStateResponse(const PlayerGameStateResponse& pos);
+    std::vector<uint8_t> encodeBeatBossResponse(const BeatBossResponse& pos);
+    std::vector<uint8_t> encodeCheckpointResponse(const CheckpointResponse& pos
+    );
+    std::vector<uint8_t>
+    encodeGameStartedResponse(const GameStartedResponse& pos);
+    std::vector<uint8_t> encodeGamePausedResponse(const GamePausedResponse& pos
+    );
+    std::vector<uint8_t> encodeGameStateResponse(const GameStateResponse& pos);
+    std::vector<uint8_t>
+    encodeEnemySpawnedResponse(const EnemySpawnedResponse& pos);
+    std::vector<uint8_t> encodeEnemyMovedResponse(const EnemyMovedResponse& pos
+    );
+    std::vector<uint8_t> encodedEnemyFiredResponse(const EnemyFiredResponse& pos
+    );
+    std::vector<uint8_t> encodeEnemyDiedResponse(const EnemyDiedResponse& pos);
+    std::vector<uint8_t> encodeCollisionResponse(const CollisionResponse& pos);
 };
 
 #endif /* !SERVER_HPP_ */
--- rtype_server/src/init_server.cpp
+++ rtype_server/src/init_server.cpp
@@ -9,7 +9,13 @@
 
 Server::Server(int p): p_(p), server_(8080, "127.0.0.1")
 {
-    std::vector<asio::ip::udp::endpoint> client_endpoint;
+    response = {};
+    response.type = 0x24;
+    response.player_id = 1;
+    response.position = {100, 200};
+    response.direction = {0, 0};
+    response.speed = 0;
+    response.timestamp.milliseconds = 0;
 
     while (1) {
         server_.poll();
@@ -20,22 +26,38 @@ Server::Server(int p): p_(p), server_(8080, "127.0.0.1")
             u_int8_t type = msg.first[0];
             auto sender = msg.second;
 
-            if (type == 0x23 && msg.first.size() >= sizeof(MoveRequest)) {
-                MoveRequest res{};
-                MoveResponse resp = recupMove(msg, res);
-                server_.send(reinterpret_cast<uint8_t*>(&resp), sizeof(resp), sender);
-            } else if (type == 0x27) {
-                PickupItemResponse resp = recupItem(msg);
-                server_.send(reinterpret_cast<uint8_t*>(&resp), sizeof(resp), sender);
-            } else if (type == 0x28) {
-                PlayerStateResponse resp = player_r(msg);
-                server_.send(reinterpret_cast<uint8_t*>(&resp), sizeof(resp), sender);
-            } else if (type == 0x29) {
-                PlayerStateResponse2 resp = player_r2(msg);
-                server_.send(reinterpret_cast<uint8_t*>(&resp), sizeof(resp), sender);
-            } else {
-                std::cerr << "Message inconnue: " << (int)type
-                    << "size= " <<  msg.first.size() << " bytes" << std::endl;
+            if (type == 0x23) {
+                MoveRequest req = decodeMoveRequest(msg.first);
+                std::cout << "Client direction = " << static_cast<int>(req.direction) << std::endl;
+
+                MoveResponse newResp = response;
+                newResp.type = 0x24;
+                newResp.player_id = 1;
+                newResp.speed = 2;
+                newResp.timestamp.milliseconds =
+                    std::chrono::duration_cast<std::chrono::milliseconds>(
+                        std::chrono::system_clock::now().time_since_epoch()).count();
+
+                switch (req.direction) {
+                    case UP:
+                        newResp.direction = {0, static_cast<uint16_t>(-1)};
+                        break;
+                    case DOWN:
+                        newResp.direction = {0, 1};
+                        break;
+                    case LEFT:
+                        newResp.direction = {static_cast<uint16_t>(-1), 0};
+                        break;
+                    case RIGHT:
+                        newResp.direction = {1, 0};
+                        break;
+                }
+
+                response = updateMoveResponse(response, newResp);
+
+
+                std::vector<uint8_t> buff = encodeMoveResponse(response);
+                server_.send(buff, buff.size(), sender);
             }
         }
     }
