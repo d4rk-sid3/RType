081d3810075237f6fad57d62f7657baf11d28cfa
FaroukJGRNT <farouk.okanla@epitech.eu>
2025-10-06 11:45:12 +0100

    fix: changed the mp3 files to ogg

--- docs/html/search/all_4.js
+++ docs/html/search/all_4.js
@@ -1,13 +1,15 @@
 var searchData=
 [
-  ['damage_0',['damage',['../structcomponent_1_1hitbox__s.html#aa328fa75249dba8908792c70b978c6ea',1,'component::hitbox_s']]],
-  ['dead_5fentities_1',['dead_entities',['../classregistry.html#ab2381c1b1b227a150443be6932f7c434',1,'registry']]],
-  ['decodeenemymovedresponse_2',['decodeEnemyMovedResponse',['../classClient.html#a9b4302efa0911b7f68cd670df4ff4c43',1,'Client']]],
-  ['decodenbrentity_3',['decodeNbrEntity',['../classClient.html#a54b89028c08e7f2c01f3c65f7b2bb10f',1,'Client']]],
-  ['decodeur_2ecpp_4',['decodeur.cpp',['../rtype__client_2src_2decodeur_8cpp.html',1,'(Global Namespace)'],['../rtype__server_2src_2decodeur_8cpp.html',1,'(Global Namespace)']]],
-  ['done_5fonce_5',['done_once',['../structcomponent_1_1animated__drawable__s.html#a585aba3d5ee3a63eed8d7e466f5a2137',1,'component::animated_drawable_s']]],
-  ['down_6',['down',['../structcomponent_1_1controllable__s.html#a81b8202fa9c13eaeee2bf8e23a4f33ad',1,'component::controllable_s']]],
-  ['draw_5fsystem_7',['draw_system',['../systems_8cpp.html#a169ac84227fdc8f7393eb5257c3f16b2',1,'draw_system(double delta, registry &amp;reg, sf::RenderWindow &amp;window, std::vector&lt; optional&lt; component::position &gt; &gt; &amp;positions, std::vector&lt; optional&lt; component::drawable &gt; &gt; &amp;draws, std::vector&lt; optional&lt; component::animated_drawable &gt; &gt; &amp;anim_draws, std::vector&lt; optional&lt; component::text &gt; &gt; &amp;texts):&#160;systems.cpp'],['../systems_8hpp.html#a169ac84227fdc8f7393eb5257c3f16b2',1,'draw_system(double delta, registry &amp;reg, sf::RenderWindow &amp;window, std::vector&lt; optional&lt; component::position &gt; &gt; &amp;positions, std::vector&lt; optional&lt; component::drawable &gt; &gt; &amp;draws, std::vector&lt; optional&lt; component::animated_drawable &gt; &gt; &amp;anim_draws, std::vector&lt; optional&lt; component::text &gt; &gt; &amp;texts):&#160;systems.cpp']]],
-  ['drawable_8',['drawable',['../namespacecomponent.html#afaa0e7d53a27314c39fd8984cd068895',1,'component']]],
-  ['drawable_5fs_9',['drawable_s',['../structcomponent_1_1drawable__s.html',1,'component']]]
+  ['d’utilisation_0',['D’utilisation',['../md__2home_2jaggie_2epitech__projects_2G-CPP-500-COT-5-1-rtype-8_2rtype__client_2include_2Client.html#autotoc_md7',1,'Exemple d’utilisation'],['../md__2home_2jaggie_2epitech__projects_2G-CPP-500-COT-5-1-rtype-8_2rtype__server_2include_2Server.html#autotoc_md17',1,'Exemple d’utilisation']]],
+  ['damage_1',['damage',['../structcomponent_1_1hitbox__s.html#aa328fa75249dba8908792c70b978c6ea',1,'component::hitbox_s']]],
+  ['dead_5fentities_2',['dead_entities',['../classregistry.html#ab2381c1b1b227a150443be6932f7c434',1,'registry']]],
+  ['decodeenemymovedresponse_3',['decodeEnemyMovedResponse',['../classClient.html#a9b4302efa0911b7f68cd670df4ff4c43',1,'Client']]],
+  ['decodenbrentity_4',['decodeNbrEntity',['../classClient.html#a54b89028c08e7f2c01f3c65f7b2bb10f',1,'Client']]],
+  ['decodeur_2ecpp_5',['decodeur.cpp',['../rtype__client_2src_2decodeur_8cpp.html',1,'(Global Namespace)'],['../rtype__server_2src_2decodeur_8cpp.html',1,'(Global Namespace)']]],
+  ['description_6',['Description',['../md__2home_2jaggie_2epitech__projects_2G-CPP-500-COT-5-1-rtype-8_2rtype__client_2include_2Client.html#autotoc_md1',1,'Description'],['../md__2home_2jaggie_2epitech__projects_2G-CPP-500-COT-5-1-rtype-8_2rtype__server_2include_2Server.html#autotoc_md9',1,'Description']]],
+  ['done_5fonce_7',['done_once',['../structcomponent_1_1animated__drawable__s.html#a585aba3d5ee3a63eed8d7e466f5a2137',1,'component::animated_drawable_s']]],
+  ['down_8',['down',['../structcomponent_1_1controllable__s.html#a81b8202fa9c13eaeee2bf8e23a4f33ad',1,'component::controllable_s']]],
+  ['draw_5fsystem_9',['draw_system',['../systems_8cpp.html#a169ac84227fdc8f7393eb5257c3f16b2',1,'draw_system(double delta, registry &amp;reg, sf::RenderWindow &amp;window, std::vector&lt; optional&lt; component::position &gt; &gt; &amp;positions, std::vector&lt; optional&lt; component::drawable &gt; &gt; &amp;draws, std::vector&lt; optional&lt; component::animated_drawable &gt; &gt; &amp;anim_draws, std::vector&lt; optional&lt; component::text &gt; &gt; &amp;texts):&#160;systems.cpp'],['../systems_8hpp.html#a169ac84227fdc8f7393eb5257c3f16b2',1,'draw_system(double delta, registry &amp;reg, sf::RenderWindow &amp;window, std::vector&lt; optional&lt; component::position &gt; &gt; &amp;positions, std::vector&lt; optional&lt; component::drawable &gt; &gt; &amp;draws, std::vector&lt; optional&lt; component::animated_drawable &gt; &gt; &amp;anim_draws, std::vector&lt; optional&lt; component::text &gt; &gt; &amp;texts):&#160;systems.cpp']]],
+  ['drawable_10',['drawable',['../namespacecomponent.html#afaa0e7d53a27314c39fd8984cd068895',1,'component']]],
+  ['drawable_5fs_11',['drawable_s',['../structcomponent_1_1drawable__s.html',1,'component']]]
 ];
--- docs/html/search/all_f.js
+++ docs/html/search/all_f.js
@@ -1,7 +1,14 @@
 var searchData=
 [
-  ['player_5fentity_5fid_0',['player_entity_id',['../client_8hpp.html#a45a5fb16434b9906d8110a57b0ca5b4d',1,'player_entity_id:&#160;client.hpp'],['../server_8hpp.html#a45a5fb16434b9906d8110a57b0ca5b4d',1,'player_entity_id:&#160;server.hpp']]],
-  ['position_1',['position',['../namespacecomponent.html#a32921f4e807a398b6ffaea1730431bec',1,'component']]],
-  ['position_5fs_2',['position_s',['../structcomponent_1_1position__s.html',1,'component']]],
-  ['position_5fsystem_3',['position_system',['../systems_8cpp.html#a48efdd22550291cb9bacb3330d67ff48',1,'position_system(double delta, registry &amp;reg, std::vector&lt; optional&lt; component::position &gt; &gt; &amp;positions, std::vector&lt; optional&lt; component::velocity &gt; &gt; &amp;velocities):&#160;systems.cpp'],['../systems_8hpp.html#a48efdd22550291cb9bacb3330d67ff48',1,'position_system(double delta, registry &amp;reg, std::vector&lt; optional&lt; component::position &gt; &gt; &amp;positions, std::vector&lt; optional&lt; component::velocity &gt; &gt; &amp;velocities):&#160;systems.cpp']]]
+  ['p_20std_3a_3astring_20address_20tt_0',['&lt;tt&gt;Client(int p, std::string address)&lt;/tt&gt;',['../md__2home_2jaggie_2epitech__projects_2G-CPP-500-COT-5-1-rtype-8_2rtype__client_2include_2Client.html#autotoc_md4',1,'']]],
+  ['pickupitemresponse_20recupitem_20tt_1',['&lt;tt&gt;PickupItemResponse recupItem(...)&lt;/tt&gt;',['../md__2home_2jaggie_2epitech__projects_2G-CPP-500-COT-5-1-rtype-8_2rtype__server_2include_2Server.html#autotoc_md13',1,'']]],
+  ['player_5fentity_5fid_2',['player_entity_id',['../client_8hpp.html#a45a5fb16434b9906d8110a57b0ca5b4d',1,'player_entity_id:&#160;client.hpp'],['../server_8hpp.html#a45a5fb16434b9906d8110a57b0ca5b4d',1,'player_entity_id:&#160;server.hpp']]],
+  ['player_5fr_20tt_3',['&lt;tt&gt;PlayerStateResponse player_r(...)&lt;/tt&gt;',['../md__2home_2jaggie_2epitech__projects_2G-CPP-500-COT-5-1-rtype-8_2rtype__server_2include_2Server.html#autotoc_md14',1,'']]],
+  ['player_5fr2_20tt_4',['&lt;tt&gt;PlayerStateResponse2 player_r2(...)&lt;/tt&gt;',['../md__2home_2jaggie_2epitech__projects_2G-CPP-500-COT-5-1-rtype-8_2rtype__server_2include_2Server.html#autotoc_md15',1,'']]],
+  ['playerstateresponse_20player_5fr_20tt_5',['&lt;tt&gt;PlayerStateResponse player_r(...)&lt;/tt&gt;',['../md__2home_2jaggie_2epitech__projects_2G-CPP-500-COT-5-1-rtype-8_2rtype__server_2include_2Server.html#autotoc_md14',1,'']]],
+  ['playerstateresponse2_20player_5fr2_20tt_6',['&lt;tt&gt;PlayerStateResponse2 player_r2(...)&lt;/tt&gt;',['../md__2home_2jaggie_2epitech__projects_2G-CPP-500-COT-5-1-rtype-8_2rtype__server_2include_2Server.html#autotoc_md15',1,'']]],
+  ['position_7',['position',['../namespacecomponent.html#a32921f4e807a398b6ffaea1730431bec',1,'component']]],
+  ['position_5fs_8',['position_s',['../structcomponent_1_1position__s.html',1,'component']]],
+  ['position_5fsystem_9',['position_system',['../systems_8cpp.html#a48efdd22550291cb9bacb3330d67ff48',1,'position_system(double delta, registry &amp;reg, std::vector&lt; optional&lt; component::position &gt; &gt; &amp;positions, std::vector&lt; optional&lt; component::velocity &gt; &gt; &amp;velocities):&#160;systems.cpp'],['../systems_8hpp.html#a48efdd22550291cb9bacb3330d67ff48',1,'position_system(double delta, registry &amp;reg, std::vector&lt; optional&lt; component::position &gt; &gt; &amp;positions, std::vector&lt; optional&lt; component::velocity &gt; &gt; &amp;velocities):&#160;systems.cpp']]],
+  ['principales_20fonctions_10',['Principales fonctions',['../md__2home_2jaggie_2epitech__projects_2G-CPP-500-COT-5-1-rtype-8_2rtype__server_2include_2Server.html#autotoc_md11',1,'']]]
 ];
